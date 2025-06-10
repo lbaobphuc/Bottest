38cf1dda172266b27e8a713869c9b226950e3828
lbaobphuc <nguyenbaophuc20080623@gmail.com>
2025-06-10 10:49:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 import telegram
-import os
 
-TOKEN = os.environ.get("8055106598:AAEWKlquZsNGrA2mKiPZ7yjqSO_9smuSNsU")
+TOKEN = "8055106598:AAEWKlquZsNGrA2mKiPZ7yjqSO_9smuSNsU"
 bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
@@ -14,10 +13,11 @@ def home():
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat.id
-    msg = update.message.text
-    bot.send_message(chat_id=chat_id, text=f"Bạn nói: {msg}")
+    if update.message:
+        chat_id = update.message.chat.id
+        msg = update.message.text
+        bot.send_message(chat_id=chat_id, text=f"Bạn nói: {msg}")
     return "ok"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
